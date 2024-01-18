6b6df86646d0a9d19161edf59ed288f85da82a5e
venom bolteop <145913044+venombolteop@users.noreply.github.com>
2024-01-18 09:59:28 +0530

    Add files via upload

---
+++ Venom/modules/stats.py
@@ -0,0 +1,18 @@
+from pyrogram import filters, Client
+from pyrogram.types import Message
+
+from Venom import OWNER, VenomX
+from Venom.database.chats import get_served_chats
+from Venom.database.users import get_served_users
+
+
+@VenomX.on_message(filters.command("stats") & filters.user(OWNER))
+async def stats(cli: Client, message: Message):
+    users = len(await get_served_users())
+    chats = len(await get_served_chats())
+    await message.reply_text(
+        f"""ᴛᴏᴛᴀʟ sᴛᴀᴛs ᴏғ {(await cli.get_me()).mention} :
+
+➻ **ᴄʜᴀᴛs :** {chats}
+➻ **ᴜsᴇʀs :** {users}"""
+    )
