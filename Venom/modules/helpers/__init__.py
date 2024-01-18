1e821fa9249165ffb79ae227410f2485f4a8e04c
venom bolteop <145913044+venombolteop@users.noreply.github.com>
2024-01-18 10:00:20 +0530

    Add files via upload

--- Venom/modules/helpers/__init__.py
+++ Venom/modules/helpers/__init__.py
@@ -1,1 +1,22 @@
+from typing import Callable
 
+from pyrogram.enums import ChatMemberStatus
+from pyrogram.types import Message
+
+from Venom import OWNER, VenomX
+
+
+def is_admins(func: Callable) -> Callable:
+    async def non_admin(c: VenomX, m: Message):
+        if m.from_user.id == OWNER:
+            return await func(c, m)
+
+        admin = await c.get_chat_member(m.chat.id, m.from_user.id)
+        if admin.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
+            return await func(c, m)
+
+    return non_admin
+
+
+from .inline import *
+from .read import *
