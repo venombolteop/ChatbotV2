692bf7c3a0375f04b792ca178ec52f394f20dc18
venom bolteop <145913044+venombolteop@users.noreply.github.com>
2025-06-01 23:02:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ API_HASH = getenv("API_HASH", "cf2a75861140ceb746c7796e07cbde9e")
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 OWNER_ID = int(getenv("OWNER_ID", "7048354045"))
 MONGO_URL = getenv("MONGO_URL", None)
-SUPPORT_GRP = getenv("SUPPORT_GRP", "Its_Venom_family")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "Itz_Venom_family")
 UPDATE_CHNL = getenv("UPDATE_CHNL", "Tomatofist")
 OWNER_USERNAME = getenv("OWNER_USERNAME", "Venom_bolte_pubIic")
 
